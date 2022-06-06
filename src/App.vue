5ba206500089aafaf56f0e47bbb31658f1c4a7be
Yu Fengjie <wb_yufengjie@meituan.com>
2022-06-06 09:26:26 +0800

    feat: del default

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<template>
+  <router-view />
+</template>
+
 <script setup>
 import { ref } from 'vue'
 
@@ -7,21 +11,6 @@ setInterval(() => {
   counter.value++
 }, 1000)
 </script>
+<style lang="less" scoped>
 
-<template>
-  <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1
-          @click="counter = 0"
-          class="text-3xl font-bold leading-tight text-gray-900"
-        >
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
-    </header>
-    <main>
-      <router-view />
-    </main>
-  </div>
-</template>
+</style>
