494e914a97de3f01feaf71eb9f2f5101bf3fb936
Yu Fengjie <wb_yufengjie@meituan.com>
2022-06-07 20:13:47 +0800

    feat: 冒泡排序,选择排序,通过left平移较难

--- src/components/grid/index.vue
+++ src/components/grid/index.vue
@@ -1,6 +1,9 @@
 <script lang="jsx">
 import { ref, watch, watchEffect } from "vue"
 import { height } from "tailwindcss/defaultTheme"
+
+import { sleep } from "@/util/index"
+
 export default {
   props: {
     arr: {
@@ -9,38 +12,108 @@ export default {
     },
   },
   setup(props) {
-    console.log("grid props", props)
     let max
     let barWidth = 30
     let gap = 10
+    let minH = 30
+    let maxH = 400
+    let arrWrap = ref([])
+    const transitionTime = 0.1 // 秒
 
-    function getHeight(val) {
-      const minH = 30
-      const maxH = 400
-      const valH = (maxH / max) * val
-      return valH < minH ? minH : valH
+    watchEffect(() => {
+      if (props.arr.length === 0) return
+      max = Math.max(...props.arr.slice(0))
+      arrWrap.value = props.arr.map((item, i) => {
+        const valH = (maxH / max) * item
+
+        return {
+          val: item,
+          height: valH < minH ? minH : parseInt(valH),
+          left: (barWidth + gap) * i,
+        }
+      })
+      console.log({ max, arrWrap })
+    })
+
+    function swap(arr, i, j, key) {
+      let temp = arr[i][key]
+      arr[i][key] = arr[j][key]
+      arr[j][key] = temp
+    }
+
+    async function handleBubbleSort() {
+      let len = arrWrap.value.length
+      for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+          arrWrap.value[j].bg = "#ff7675"
+          arrWrap.value[j + 1].bg = "#ff7675"
+
+          if (arrWrap.value[j].val > arrWrap.value[j + 1].val) {
+            swap(arrWrap.value, j, j+1, 'height')
+            swap(arrWrap.value, j, j + 1, "val")
+            // swap(arrWrap.value, j, j + 1, "left")
+          }
+          await sleep(transitionTime * 1000)
+          arrWrap.value[j].bg = "#0984e3"
+          arrWrap.value[j + 1].bg = "#0984e3"
+        }
+      }
+    }
+
+    async function handleSelectSort() {
+      let len = arrWrap.value.length
+      for (let i = 0; i < len; i++) {
+        let minIndex = i
+        let min = arrWrap.value[i].val
+        for (let j = i+1; j < len; j++) {
+          arrWrap.value[i].bg = "#ff7675"
+          arrWrap.value[j].bg = "#ff7675"
+
+          if (arrWrap.value[j].val < min) {
+            minIndex = j
+            min = arrWrap.value[j].val
+          }
+          await sleep(transitionTime * 1000)
+          arrWrap.value[j].bg = "#0984e3"
+        }
+        swap(arrWrap.value, i, minIndex, 'height')
+        swap(arrWrap.value, i, minIndex, "val")
+        arrWrap.value[i].bg = "#0984e3"
+      }
     }
 
     return () => {
-      max = Math.max(...props.arr)
-      console.log({ max, arr: props.arr })
 
       return (
-        <div class="gridCon">
-          {props.arr.map((item, i) => {
-            const height = parseInt(getHeight(item))
-            const styleObj = {
-              width: `${barWidth}px`,
-              height: `${height}px`,
-              left: `${(barWidth + gap) * i}px`,
-            }
-            return (
-              <div style={styleObj} class="item">
-                {item}
-              </div>
-            )
-          })}
-        </div>
+        <>
+          <div class="gridCon" style={{"--transitionTime": `${transitionTime}s`}}>
+            {arrWrap.value.map((item, i) => {
+              const styleObj = {
+                width: `${barWidth}px`,
+                height: `${item.height}px`,
+                left: `${item.left}px`,
+                background: item.bg
+              }
+              return (
+                <div style={styleObj} class="item">
+                  {item.val}
+                </div>
+              )
+            })}
+          </div>
+          <button
+            class="rounded bg-gray-900 text-white p-1 my-2 mr-2"
+            onClick={handleBubbleSort}
+          >
+            冒泡排序
+          </button>
+          <button
+            class="rounded bg-gray-900 text-white p-1 my-2 mr-2"
+            onClick={handleSelectSort}
+          >
+            选择排序
+          </button>
+        </>
       )
     }
   },
@@ -58,7 +131,8 @@ export default {
     background: #0984e3;
     color: #fff;
     text-align: center;
-    transition: 1s;
+    transition: height var(--transitionTime) ease-in;
+    // transition: left 0.3s;
   }
 }
 </style>
--- src/views/BubbleSort/index.vue
+++ src/views/BubbleSort/index.vue
@@ -19,54 +19,31 @@
     </button>
     <div>你的输入:{{ `[${inputArr.join(", ")}]` }}</div>
     <Grid :arr="inputArr" />
-    <button class="rounded bg-gray-900 text-white p-1 my-2" @click="handleSort">
-      冒泡排序
-    </button>
   </div>
 </template>
 
 <script setup>
-import { computed, ref, watch } from "vue"
-import { createRandomArr, sleep } from "@/util/index"
+import { ref, watch, watchEffect } from "vue"
+import { createRandomArr } from "@/util/index"
 
 import Grid from "@/components/grid/index.vue"
 
-const inputStr = ref("10,80")
-const inputArr = ref([10, 80])
+const inputStr = ref("")
+const inputArr = ref([])
 
 function updateInputArr() {
-  let arr = inputStr.value.split(",").map((item) => parseInt(item))
-  // inputArr.value.length = 0
-  // arr.forEach(item=>{
-  //     inputArr.value.push(item)
-  // })
-  inputArr.value = arr
+  if (inputStr.value === "") return
+  inputArr.value = inputStr.value.split(",").map((item) => parseInt(item))
 }
 
 updateInputArr()
-watch(inputStr, () => {
+watchEffect(() => {
   updateInputArr()
 })
 handleCreateArr()
 
 function handleCreateArr() {
   inputStr.value = createRandomArr(20).join(",")
-}
-function swap(arr, i, j) {
-  let temp = arr[i]
-  arr[i] = arr[j]
-  arr[j] = temp
-}
-async function handleSort() {
-  let len = inputArr.value.length
-  for (let i = 0; i < len - 1; i++) {
-    for (let j = 0; j < len - 1 - i; j++) {
-      if (inputArr.value[j] > inputArr.value[j + 1]) {
-        swap(inputArr.value, j, j + 1)
-        await sleep(1000)
-      }
-    }
-  }
 }
 </script>
 
