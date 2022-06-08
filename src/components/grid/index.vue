46505045ed4ee28995a9d7fb3246afafa5c5df5f
Yu Fengjie <wb_yufengjie@meituan.com>
2022-06-08 14:20:09 +0800

    feat: 选择排序

--- src/components/grid/index.vue
+++ src/components/grid/index.vue
@@ -17,82 +17,127 @@ export default {
     let gap = 10
     let minH = 30
     let maxH = 400
-    let arrWrap = ref([])
+    let arrWrap1 = ref([]) // {id,val} 排序的数组
+    let arrWrap2 = ref([]) // {id,val,height,left} // 通过id与arrWrap1关联,索引顺序不变,left在变
     const transitionTime = 0.1 // 秒
+
+    const red = "#ff7675"
+    const blue = "#0984e3"
+    const yellow = "#fdcb6e"
 
     watchEffect(() => {
       if (props.arr.length === 0) return
       max = Math.max(...props.arr.slice(0))
-      arrWrap.value = props.arr.map((item, i) => {
+      arrWrap1.value = props.arr.map((item, i) => {
+        return {
+          id: i,
+          val: item,
+        }
+      })
+      arrWrap2.value = props.arr.map((item, i) => {
         const valH = (maxH / max) * item
 
         return {
+          id: i,
           val: item,
           height: valH < minH ? minH : parseInt(valH),
           left: (barWidth + gap) * i,
         }
       })
-      console.log({ max, arrWrap })
     })
 
-    function swap(arr, i, j, key) {
-      let temp = arr[i][key]
-      arr[i][key] = arr[j][key]
-      arr[j][key] = temp
+    function swap(arr, item1, item2, key) {
+      if (item1 === item2) return
+      let temp = item1[key]
+      item1[key] = item2[key]
+      item2[key] = temp
     }
 
     async function handleBubbleSort() {
-      let len = arrWrap.value.length
+      let len = arrWrap1.value.length
       for (let i = 0; i < len - 1; i++) {
         for (let j = 0; j < len - 1 - i; j++) {
-          arrWrap.value[j].bg = "#ff7675"
-          arrWrap.value[j + 1].bg = "#ff7675"
+          let item1 = arrWrap1.value[j]
+          let item2 = arrWrap1.value[j + 1]
 
-          if (arrWrap.value[j].val > arrWrap.value[j + 1].val) {
-            swap(arrWrap.value, j, j+1, 'height')
-            swap(arrWrap.value, j, j + 1, "val")
-            // swap(arrWrap.value, j, j + 1, "left")
+          let item3 = arrWrap2.value.find((item) => item.id === item1.id)
+          let item4 = arrWrap2.value.find((item) => item.id === item2.id)
+
+          item3.bg = red
+          item4.bg = red
+
+          if (item1.val > item2.val) {
+            swap(arrWrap1.value, item1, item2, "id")
+            swap(arrWrap1.value, item1, item2, "val")
+            swap(arrWrap2.value, item3, item4, "left")
           }
+
           await sleep(transitionTime * 1000)
-          arrWrap.value[j].bg = "#0984e3"
-          arrWrap.value[j + 1].bg = "#0984e3"
+          item3.bg = blue
+          item4.bg = blue
         }
       }
     }
 
+    /**
+     * item1-->item3
+     * item2-->item4
+     * itemMin-->item5
+     */
     async function handleSelectSort() {
-      let len = arrWrap.value.length
-      for (let i = 0; i < len; i++) {
-        let minIndex = i
-        let min = arrWrap.value[i].val
-        for (let j = i+1; j < len; j++) {
-          arrWrap.value[i].bg = "#ff7675"
-          arrWrap.value[j].bg = "#ff7675"
+      let len = arrWrap1.value.length
+      for (let i = 0; i < len - 1; i++) {
+        let item1 = arrWrap1.value[i]
+        let itemMin = item1
 
-          if (arrWrap.value[j].val < min) {
-            minIndex = j
-            min = arrWrap.value[j].val
+        let item3 = arrWrap2.value.find((item) => item.id === item1.id)
+        item3.bg = red
+
+        let item4
+        let item5 = arrWrap2.value.find((item) => item.id === itemMin.id)
+
+        for (let j = i + 1; j < len; j++) {
+          let item2 = arrWrap1.value[j]
+          item4 = arrWrap2.value.find((item) => item.id === item2.id)
+          item4.bg = item4.id === itemMin.id ? yellow : red
+
+          if (item2.val < itemMin.val) {
+            item5.bg = blue
+
+            itemMin = item2
+            item5 = arrWrap2.value.find((item) => item.id === itemMin.id)
+
+            item5.bg = yellow
           }
+
           await sleep(transitionTime * 1000)
-          arrWrap.value[j].bg = "#0984e3"
+
+          item4.bg = blue
         }
-        swap(arrWrap.value, i, minIndex, 'height')
-        swap(arrWrap.value, i, minIndex, "val")
-        arrWrap.value[i].bg = "#0984e3"
+
+        swap(arrWrap1.value, item1, itemMin, "id")
+        swap(arrWrap1.value, item1, itemMin, "val")
+        swap(arrWrap2.value, item3, item5, "left")
+
+        item3.bg = blue
       }
+      // console.log(arrWrap1.value.map(item=>item.val));
+      // console.log(arrWrap2.value.sort((a,b)=>a.left-b.left));
     }
 
     return () => {
-
       return (
         <>
-          <div class="gridCon" style={{"--transitionTime": `${transitionTime}s`}}>
-            {arrWrap.value.map((item, i) => {
+          <div
+            class="gridCon"
+            style={{ "--transitionTime": `${transitionTime}s` }}
+          >
+            {arrWrap2.value.map((item, i) => {
               const styleObj = {
                 width: `${barWidth}px`,
                 height: `${item.height}px`,
                 left: `${item.left}px`,
-                background: item.bg
+                background: item.bg,
               }
               return (
                 <div style={styleObj} class="item">
@@ -131,7 +176,7 @@ export default {
     background: #0984e3;
     color: #fff;
     text-align: center;
-    transition: height var(--transitionTime) ease-in;
+    transition: left var(--transitionTime) ease-in;
     // transition: left 0.3s;
   }
 }
